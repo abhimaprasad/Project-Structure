695bc96dffeadcbfe1cd924b9e1ce6cf54a470ce
Abhima Prasad <69728750+abhimaprasad@users.noreply.github.com>
2026-04-29 23:09:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function toggleMode() {
-  document.body.classList.toggle("dark");
-}
+gsap.registerPlugin(ScrollTrigger);
+
+/* Fade Up Animation */
+gsap.utils.toArray(".fade-up").forEach((el) => {
+  gsap.from(el, {
+    scrollTrigger: {
+      trigger: el,
+      start: "top 85%",
+    },
+    y: 60,
+    opacity: 0,
+    duration: 1
+  });
+});
